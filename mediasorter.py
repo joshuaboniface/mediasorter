98937db809af870d78b9802e6fc350b443382b23
Joshua M. Boniface <joshua@boniface.me>
2022-12-24 02:00:17 -0500

    Apply overrides after removing "the"
    Allows an override to forcibly re-add the "the" if required to find the
    right movie (e.g. "terminator" -> "the+terminator")

--- mediasorter.py
+++ mediasorter.py
@@ -225,10 +225,11 @@ def sort_movie_file(config, srcpath, dstpath, metainfo_tag):
     # Series title: start to year_idx
     raw_movie_title = split_filename[0:year_idx]
     search_movie_title = '+'.join([x.lower() for x in raw_movie_title])
-    if search_movie_title in config['search_overrides']:
-        search_movie_title = config['search_overrides'][search_movie_title]
     # Remove the first "The" from the title when searching to avoid weird conflicts
     search_movie_title = re.sub('[Tt]he\+', '', search_movie_title, 1)
+    # Apply overrides
+    if search_movie_title in config['search_overrides']:
+        search_movie_title = config['search_overrides'][search_movie_title]
     logger(config, "Raw file info:    movie='{}' year={}".format(search_movie_title, search_movie_year))
 
     # Fetch series information from TVMaze
